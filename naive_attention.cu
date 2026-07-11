e21704d1a38a5794eec49df0eb309f6236f7d1f2
Monish_sri_sai_devineni <monish6468@gmail.com>
2026-07-11 04:03:53 -0400

    added v matrix

--- naive_attention.cu
+++ naive_attention.cu
@@ -49,31 +49,36 @@ double get_time() {
 }
 
 int main() {
-    float *h_A, *h_B, *h_C_cpu, *h_C_gpu;
-    float *d_A, *d_B, *d_C;
+    float *h_A, *h_B, *h_C_cpu, *h_C_gpu, *h_V;
+    float *d_A, *d_B, *d_C, *d_V;
     int size_A = M * K * sizeof(float);
     int size_B = K * N * sizeof(float);
     int size_C = M * N * sizeof(float);
+    int size_V = M * N * sizeof(float);
 
     // Allocate host memory
     h_A = (float*)malloc(size_A);
     h_B = (float*)malloc(size_B);
     h_C_cpu = (float*)malloc(size_C);
     h_C_gpu = (float*)malloc(size_C);
+    h_V = (float*)malloc(size_V);
 
     // Initialize matrices
     srand(time(NULL));
     init_matrix(h_A, M, K);
     init_matrix(h_B, K, N);
+    init_matrix(h_V, M, N);
 
     // Allocate device memory
     cudaMalloc(&d_A, size_A);
     cudaMalloc(&d_B, size_B);
     cudaMalloc(&d_C, size_C);
+    cudaMalloc(&d_V, size_V);
 
     // Copy data to device
     cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_V, h_V, size_V, cudaMemcpyHostToDevice);
 
     // Define grid and block dimensions
     dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE);
@@ -106,9 +111,11 @@ int main() {
     free(h_B);
     free(h_C_cpu);
     free(h_C_gpu);
+    free(h_V);
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
+    cudaFree(d_V);
 
     return 0;
 }
